34ec7157dfd11714b9506fa5e8fd1d5e7f0c23f4
Karthik1224 <durgamkarthik150@gmail.com>
2023-07-26 12:30:11 +0530

    final changes made

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -16,25 +16,25 @@ public class BankAccount {
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
-        int maxPossibleSum = 9 * digits;
-        if (sum <0 || sum > maxPossibleSum)
+        if(sum > digits*9)
         {
             throw new Exception("Account Number can not be generated");
         }
-
-        // Generate the account number
-        char[] accountNumber = new char[digits];
-        for (int i = 0; i < digits; i++)
+        String accountNumber = "";
+        for(int i=0 ; i<digits ; i++)
         {
-            // Determine the current digit value
-            int digitValue = Math.min(9, sum);
-            accountNumber[i] = (char)('0' + digitValue);
-
-            // Reduce the remaining sum
-            sum -= digitValue;
+            if(sum > 9)
+            {
+                accountNumber += 9;
+                sum -= 9;
+            }
+            else
+            {
+                accountNumber += sum;
+                sum = 0;
+            }
         }
-
-        return new String(accountNumber);
+        return accountNumber;
 
     }
 
@@ -48,7 +48,7 @@ public class BankAccount {
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
 
-        if((balance - amount) < minBalance)
+        if((this.balance - amount) < minBalance)
         {
             throw new Exception("Insufficient Balance");
         }
--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -15,12 +15,13 @@ public class SavingsAccount extends BankAccount{
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
 
-        if(amount <= maxWithdrawalLimit)
+        if(amount > maxWithdrawalLimit)
         {
-           super.withdraw(amount);
+            throw new Exception("Maximum Withdraw Limit Exceeded");
         }
-        else {
-            throw new Exception("Maximum Withdraw Limit Exceed");
+        else
+        {
+            super.withdraw(amount);
         }
 
 
