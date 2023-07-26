49dc157df3692b2be32541716cab07120de40f58
Karthik1224 <durgamkarthik150@gmail.com>
2023-07-26 11:46:09 +0530

    changes made

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -5,6 +5,8 @@ public class CurrentAccount extends BankAccount{
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
+        super(name,balance,5000);
+        this.tradeLicenseId = tradeLicenseId;
 
     }
 
@@ -13,7 +15,72 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
+        boolean flag = validate(tradeLicenseId);
+        if(flag == false)
+        {
+            String rearranged = reorganise(tradeLicenseId);
+            if(rearranged.isEmpty())
+            {
+                throw new Exception("Valid License can not be generated");
+            }
+            else
+            {
+                this.tradeLicenseId = rearranged;
+            }
+        }
 
+    }
+
+    public boolean validate (String s)
+    {
+        for(int i=0; i<s.length()-1; i++)
+        {
+            if(s.charAt(i)==s.charAt(i+1))
+            {
+               return false;
+            }
+        }
+        return true;
+    }
+
+    public String reorganise(String s)
+    {
+        int[]fre = new int[26];
+        for(char ch : s.toCharArray())
+        {
+            fre[ch-'A']++;
+        }
+        int max = 0, letter=0;
+        for(int i=0; i<26; i++)
+        {
+            if(fre[i]>max)
+            {
+                max = fre[i];
+                letter=i;
+            }
+        }
+        int n = s.length();
+        if(max > (n+1)/2) return "";
+        char[]ans = new char[n];
+        int ind=0;
+        for(int i=0; i<max; i++)
+        {
+            ans[ind] = (char)(letter+'A');
+            ind = ind+2;
+            fre[letter]--;
+        }
+
+        for(int i=0; i<26; i++)
+        {
+            while(fre[i]>0)
+            {
+                if(ind >= n) ind=1;
+                ans[ind] = (char)(i+'A');
+                ind += 2;
+                fre[i]--;
+            }
+        }
+        return String.valueOf(ans);
     }
 
 }
--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -6,23 +6,64 @@ public class SavingsAccount extends BankAccount{
 
     public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
         // minimum balance is 0 by default
-
+       super(name,balance,0);
+       this.maxWithdrawalLimit = maxWithdrawalLimit;
+       this.rate = rate;
     }
     public void withdraw(double amount) throws Exception {
         // Might throw the following errors:
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
 
+        if(amount > maxWithdrawalLimit)
+        {
+            throw new Exception("Maximum Withdraw Limit Exceed");
+        }
+        else if(amount > getBalance())
+        {
+            throw new Exception("Insufficient Balance");
+        }
+
     }
 
     public double getSimpleInterest(int years){
         // Return the final amount considering that bank gives simple interest on current amount
+        double principal = super.getBalance();
+        double simpleInterest = principal * rate * years;
+
+        // Calculate the final amount
+        double finalAmount = principal + simpleInterest;
+
+        return finalAmount;
 
     }
 
     public double getCompoundInterest(int times, int years){
         // Return the final amount considering that bank gives compound interest on current amount given times per year
 
+        double principal = super.getBalance();
+        double compoundInterest = principal * Math.pow((1 + (rate / times)), times * years) - principal;
+
+        // Calculate the final amount
+        double finalAmount = principal + compoundInterest;
+
+        return finalAmount;
+
     }
 
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public double getMaxWithdrawalLimit() {
+        return maxWithdrawalLimit;
+    }
+
+    public void setMaxWithdrawalLimit(double maxWithdrawalLimit) {
+        this.maxWithdrawalLimit = maxWithdrawalLimit;
+    }
 }
